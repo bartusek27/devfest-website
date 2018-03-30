b52ffa704365e8bb6cb9571631ada7ea89b1ad6c
Václav Pavlíček <vaclav@pavlicek.cz>
2018-03-30 13:14:40 +0200

    add animation and spinner to speaker-card

--- src/app/components/speaker-card/speaker-card.component.ts
+++ src/app/components/speaker-card/speaker-card.component.ts
@@ -6,11 +6,31 @@ import {Organizer} from "../../database/organizer";
 import {Speaker} from "../../database/speaker";
 import {AngularFirestore} from "angularfire2/firestore";
 import {AngularFireStorage} from "angularfire2/storage";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-speaker-card',
   templateUrl: './speaker-card.component.html',
-  styleUrls: ['./speaker-card.component.scss']
+  styleUrls: ['./speaker-card.component.scss'],
+  animations: [
+    trigger(
+      'slideInRight',
+      [
+        transition(
+          ':enter', [
+            style({transform: 'translateY(100%)', opacity: 0}),
+            animate('500ms', style({transform: 'translateY(0)', opacity: 1}))
+          ]
+        ),
+        transition(
+          ':leave', [
+            style({transform: 'translateY(0)', 'opacity': 1}),
+            animate('500ms', style({transform: 'translateY(-100%)', opacity: 0}))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class SpeakerCardComponent implements OnInit {
 
@@ -33,6 +53,7 @@ export class SpeakerCardComponent implements OnInit {
   ngOnInit() {
     this.fireStore.collection<Speaker>('speakers').doc<Speaker>(this.id).valueChanges().subscribe((data) => {
       this.speaker = data;
+      console.log(this.speaker.photo);
       const ref = this.storage.ref(this.speaker.photo);
       this.profileUrl = ref.getDownloadURL();
     });
